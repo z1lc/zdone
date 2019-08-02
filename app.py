1fcc806a9cfb1737e2a914fd8ac46378e848fb3e
rsanek <rsanek@gmail.com>
2019-08-01 19:02:20 -0700

    connect to postgres

--- app.py
+++ app.py
@@ -1,9 +1,30 @@
-from flask import Flask
+import os
 from datetime import datetime
+
+import psycopg2
+from flask import Flask
+
 app = Flask(__name__)
+DATABASE_URL = os.environ['DATABASE_URL']
+
 
 @app.route('/')
 def homepage():
+    conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+    # create a cursor
+    cur = conn.cursor()
+
+    # execute a statement
+    print('PostgreSQL database version:')
+    cur.execute('SELECT version()')
+
+    # display the PostgreSQL database server version
+    db_version = cur.fetchone()
+    print(db_version)
+
+    # close the communication with the PostgreSQL
+    cur.close()
+
     the_time = datetime.now().strftime("%A, %d %b %Y")
 
     return """
@@ -12,6 +33,7 @@ def homepage():
 
     <img src="http://loremflickr.com/600/400">
     """.format(time=the_time)
+
 
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
