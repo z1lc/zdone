3742792298dfbe42eaf241d0d9dead57b7749685
rsanek <rsanek@gmail.com>
2019-08-01 19:03:34 -0700

    print

--- app.py
+++ app.py
@@ -11,28 +11,20 @@ DATABASE_URL = os.environ['DATABASE_URL']
 @app.route('/')
 def homepage():
     conn = psycopg2.connect(DATABASE_URL, sslmode='require')
-    # create a cursor
     cur = conn.cursor()
-
-    # execute a statement
     print('PostgreSQL database version:')
     cur.execute('SELECT version()')
-
-    # display the PostgreSQL database server version
     db_version = cur.fetchone()
-    print(db_version)
-
-    # close the communication with the PostgreSQL
     cur.close()
 
     the_time = datetime.now().strftime("%A, %d %b %Y")
 
     return """
     <h1>Hello heroku</h1>
-    <p>It is currently {time}.</p>
+    <p>It is currently {time}. Db version is {version}</p>
 
     <img src="http://loremflickr.com/600/400">
-    """.format(time=the_time)
+    """.format(time=the_time, version=db_version)
 
 
 if __name__ == '__main__':
