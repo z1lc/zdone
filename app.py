3d1f9d2f79b70bb18716d92f4a7296d23f1d6c54
rsanek <rsanek@gmail.com>
2019-08-02 19:27:04 -0700

    time test

--- app.py
+++ app.py
@@ -12,19 +12,27 @@ conn = psycopg2.connect(DATABASE_URL, sslmode='require')
 @app.route('/')
 def homepage():
     the_time = datetime.now().strftime("%A, %d %b %Y")
+    put('time', the_time)
 
     return """
     <h1>Hello heroku</h1>
     <p>It is currently {time}. Db version is {version}</p>
-    """.format(time=the_time, version=get('TOODLEDO_CLIENT_ID'))
+    """.format(time=the_time, version=get('time'))
 
 
 def get(key):
     cur = conn.cursor()
-    cur.execute("SELECT v FROM zkv WHERE k='{key}'".format(key=key))
-    res = cur.fetchone()
+    cur.execute("SELECT v FROM kv WHERE k='{key}'".format(key=key))
+    res = cur.fetchone()[0]
     cur.close()
     return res
+
+
+def put(key, value):
+    cur = conn.cursor()
+    cur.execute("INSERT INTO kv(k, v) VALUES ({key}, {value}) ON CONFLICT (k) DO UPDATE SET v = {value}"
+                .format(key=key, value=value))
+    cur.close()
 
 
 if __name__ == '__main__':
