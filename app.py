fca56d90944fd62d3b1efda20d7f9529ab93537a
rsanek <rsanek@gmail.com>
2019-08-03 16:47:28 -0700

    render with template

--- app.py
+++ app.py
@@ -1,28 +1,25 @@
-from flask import Flask
+from flask import Flask, render_template
 from toodledo import Toodledo
 
 import kv
 from storage import TokenStoragePostgres
 
 app = Flask(__name__)
+toodledo = Toodledo(
+    clientId=kv.get('TOODLEDO_CLIENT_ID'),
+    clientSecret=kv.get('TOODLEDO_CLIENT_SECRET'),
+    tokenStorage=TokenStoragePostgres("TOODLEDO_TOKEN_JSON"),
+    scope="basic tasks notes outlines lists share write folders")
 
 
 @app.route('/')
 def homepage():
-    storage = TokenStoragePostgres("TOODLEDO_TOKEN_JSON")
-    toodledo = Toodledo(
-        clientId=kv.get('TOODLEDO_CLIENT_ID'),
-        clientSecret=kv.get('TOODLEDO_CLIENT_SECRET'),
-        tokenStorage=storage,
-        scope="basic tasks notes outlines lists share write folders")
-    account = toodledo.GetAccount()
-
     to_print = ""
     for task in toodledo.GetTasks(params={"fields": "length,repeat,parent"}):
         if task.completedDate is None and (task.length != 0 or hasattr(task, "repeat")):
             to_print += task.title + '  ' + '<br>'
 
-    return to_print
+    return render_template('index.html', username="fdsfds")
 
 
 if __name__ == '__main__':
