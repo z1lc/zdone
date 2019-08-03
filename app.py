8c14db0c7d4e694b40252814bd46746ee8dcba4c
rsanek <rsanek@gmail.com>
2019-08-02 19:34:56 -0700

    try Psycopg conversion

--- app.py
+++ app.py
@@ -5,8 +5,6 @@ import psycopg2
 from flask import Flask
 
 app = Flask(__name__)
-DATABASE_URL = os.environ['DATABASE_URL']
-conn = psycopg2.connect(DATABASE_URL, sslmode='require')
 
 
 @app.route('/')
@@ -20,19 +18,28 @@ def homepage():
     """.format(time=the_time, version=get('time'))
 
 
+def get_con():
+    return psycopg2.connect(os.environ['DATABASE_URL'], sslmode='require')
+
+
 def get(key):
+    conn = get_con()
     cur = conn.cursor()
     cur.execute("SELECT v FROM kv WHERE k='{key}'".format(key=key))
     res = cur.fetchone()[0]
     cur.close()
+    conn.close()
     return res
 
 
 def put(key, value):
+    conn = get_con()
     cur = conn.cursor()
-    cur.execute("INSERT INTO kv(k, v) VALUES ('{key}', '{value}') ON CONFLICT (k) DO UPDATE SET v = '{value}'"
+    cur.execute("INSERT INTO kv(k, v) VALUES (%s, %s) ON CONFLICT (k) DO UPDATE SET v = %s"
                 .format(key=key, value=value))
+    conn.commit()
     cur.close()
+    conn.close()
 
 
 if __name__ == '__main__':
