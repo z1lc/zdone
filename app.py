cc27ac01cd62ee1a8836ac8c11b3cc622219d0cf
rsanek <rsanek@gmail.com>
2019-08-07 12:05:48 -0700

    add basic intro, refine design, start implementing habitica

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from typing import List
 
 import pytz
 from flask import Flask, render_template, request
+from habitipy import Habitipy
 from toodledo import Toodledo
 
 import kv
@@ -14,7 +15,24 @@ toodledo = Toodledo(
     clientSecret=kv.get('TOODLEDO_CLIENT_SECRET'),
     tokenStorage=TokenStoragePostgres("TOODLEDO_TOKEN_JSON"),
     scope="basic tasks notes outlines lists share write folders")
+habitica = Habitipy({
+    'url': 'https://habitica.com',
+    'login': kv.get('HABITICA_USER_ID'),
+    'password': kv.get('HABITICA_API_TOKEN'),
+    'show_numbers': 'y',
+    'show_style': 'wide'
+})
 today = datetime.datetime.now(pytz.timezone('US/Pacific')).date()
+cached_tasks = []
+
+
+@app.route('/habitica')
+def habitica_page():
+    habit_list = []
+    for habit in habitica.tasks.user.get(type='dailys'):
+        habit_list.append(habit["text"])
+        # habitica.tasks[habit["id"]].score.up.post()
+    return str(habit_list)
 
 
 @app.route('/prioritize')
@@ -29,7 +47,7 @@ def show_prioritized_list():
 def get_sorted_and_unsorted_tasks() -> (List, List):
     currently_sorted_in_db = kv.get("priorities").split("|||")
     sorted_tasks, unsorted_tasks = [], []
-    all_tasks = toodledo.GetTasks(params={"fields": "duedate,star,length,repeat,parent"})
+    all_tasks = get_all_tasks_from_api()
     all_recurring_tasks = [t for t in all_tasks if t.completedDate is None and t.length != 0]
     task_map = {t.title: t for t in all_recurring_tasks}
     for name in currently_sorted_in_db:
@@ -49,23 +67,34 @@ def update_priorities():
     return "{'result': 'success'}"
 
 
+def get_all_tasks_from_api():
+    global cached_tasks
+    if not cached_tasks:
+        cached_tasks = toodledo.GetTasks(params={"fields": "duedate,star,length,repeat,parent"})
+    return cached_tasks
+
+
 @app.route('/')
 def homepage():
     minutes_completed_today = 0
-    all_tasks = toodledo.GetTasks(params={"fields": "length"})
+    tasks_completed, tasks_to_do, tasks_backlog = [], [], []
+    all_tasks = get_all_tasks_from_api()
     for task in all_tasks:
-        if task.completedDate == today:
+        if task.completedDate == today and task.length > 0:
             minutes_completed_today += task.length
+            tasks_completed.append(task)
     minutes_left_to_schedule = 120 - minutes_completed_today
     tasks, _ = get_sorted_and_unsorted_tasks()
     i = 0
-    tasks_to_do = []
     minutes_allocated = 0
     while minutes_left_to_schedule > 0 and i < len(tasks):
-        if tasks[i].dueDate <= today and tasks[i].length <= (minutes_left_to_schedule + 5):
-            tasks_to_do.append(tasks[i])
-            minutes_left_to_schedule -= tasks[i].length
-            minutes_allocated += tasks[i].length
+        if tasks[i].dueDate <= today:
+            if tasks[i].length <= (minutes_left_to_schedule + 5):
+                tasks_to_do.append(tasks[i])
+                minutes_left_to_schedule -= tasks[i].length
+                minutes_allocated += tasks[i].length
+            else:
+                tasks_backlog.append(tasks[i])
         i += 1
 
     times = {
@@ -74,7 +103,9 @@ def homepage():
         'minutes_allocated': minutes_allocated
     }
     return render_template('index.html',
-                           tasks=tasks_to_do,
+                           tasks_completed=tasks_completed,
+                           tasks_to_do=tasks_to_do,
+                           tasks_backlog=tasks_backlog,
                            times=times)
 
 
