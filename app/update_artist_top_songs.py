383b8ea6674a1181bef84e5cb65112cec7c29ec0
Robert Sanek <rsanek@gmail.com>
2020-05-04 22:29:15 -0500

    Add runtime specification and add retries into artist refresh

--- app/update_artist_top_songs.py
+++ app/update_artist_top_songs.py
@@ -1,9 +1,12 @@
 from typing import List
 
 from sentry_sdk import capture_exception
+from spotipy import SpotifyException
 
 from app.models import SpotifyArtist, User
 from app.spotify import get_spotify, refresh_top_tracks
+
+MAX_RETRIES_PER_ARTIST = 2
 
 # This code is scheduled to run once daily by the Heroku Scheduler, to avoid having to do this in-request.
 if __name__ == '__main__':
@@ -16,13 +19,20 @@ if __name__ == '__main__':
     print(f'Getting top liked songs as user {user.username}...')
 
     for i, artist in enumerate(artists):
-        try:
-            dropped, top_tracks = refresh_top_tracks(sp, artist.uri)
-            print(f'[{round(i / len(artists) * 100)}%] '
-                  f'Updated mappings for artist {artist.name}, dropping {dropped} & adding {len(top_tracks)}.')
-        except Exception as e:
-            print(f'Received exception while trying to get top tracks for artist {artist.name}.')
-            print('Exception was sent to Sentry.')
-            capture_exception(e)
+        try_count = 0
+        while try_count < MAX_RETRIES_PER_ARTIST:
+            try_count += 1
+            try:
+                dropped, top_tracks = refresh_top_tracks(sp, artist.uri)
+                print(f'[{round(i / len(artists) * 100)}%] '
+                      f'Updated mappings for artist {artist.name}, dropping {dropped} & adding {len(top_tracks)}.')
+            except SpotifyException as probably_token_expiration:
+                print(f'Received Spotify exception {probably_token_expiration}. '
+                      f'Will try to refresh access token and try again.')
+                sp = get_spotify("zdone", user)
+            except Exception as e:
+                print(f'Received exception while trying to get top tracks for artist {artist.name}.')
+                print('Exception was sent to Sentry.')
+                capture_exception(e)
 
     print('Updated all artists. Exiting...')
