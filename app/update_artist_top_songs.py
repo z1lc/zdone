d9377deed8283f79c733f5f6a4c91f6ec4307d33
Robert Sanek <rsanek@gmail.com>
2020-05-04 22:32:39 -0500

    capture exception on last try

--- app/update_artist_top_songs.py
+++ app/update_artist_top_songs.py
@@ -30,8 +30,12 @@ if __name__ == '__main__':
                 print(f'Received Spotify exception {probably_token_expiration}. '
                       f'Will try to refresh access token and try again.')
                 sp = get_spotify("zdone", user)
+                if try_count == MAX_RETRIES_PER_ARTIST:
+                    # looks like we weren't able to resolve this with just a token refresh.
+                    # publish this to Sentry so we know what's going on.
+                    capture_exception(probably_token_expiration)
             except Exception as e:
-                print(f'Received exception while trying to get top tracks for artist {artist.name}.')
+                print(f'Received unhandled exception while trying to get top tracks for artist {artist.name}.')
                 print('Exception was sent to Sentry.')
                 capture_exception(e)
 
