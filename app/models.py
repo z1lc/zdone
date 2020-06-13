278d2f7e5a4611c3547affcaaa428f4cc7df9a12
Robert Sanek <rsanek@gmail.com>
2020-06-13 10:29:53 -0500

    Add tasks & task_logs tables

--- app/models.py
+++ app/models.py
@@ -21,6 +21,7 @@ class User(UserMixin, BaseModel):
     username: str = db.Column(db.String(64), index=True, unique=True)
     email: str = db.Column(db.String(128), index=True, unique=True)
     password_hash: str = db.Column(db.String(128))
+    # one of https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
     current_time_zone: str = db.Column(db.String(128), nullable=True)
 
     maximum_minutes_per_day: int = db.Column(db.Integer, nullable=False, server_default='120')
@@ -189,3 +190,38 @@ class ReminderNotification(BaseModel):
     reminder_id: int = db.Column(db.Integer, db.ForeignKey('reminders.id'), nullable=False)
     sent_at: datetime.datetime = db.Column(db.DateTime, nullable=False)
     sent_via = db.Column(db.String, nullable=False)
+
+
+class Task(BaseModel):
+    __tablename__ = "tasks"
+    id: int = db.Column(db.Integer, primary_key=True)
+    user_id: int = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    title: str = db.Column(db.Text, nullable=False)
+    description: str = db.Column(db.Text, nullable=True)
+    ideal_interval: int = db.Column(db.Integer, nullable=False)
+    # the local date of last completion, for ease of skew calculation
+    last_completion: datetime.date = db.Column(db.Date, nullable=False)
+    # local date for when this task can be re-enabled again
+    defer_until: datetime.date = db.Column(db.Date, nullable=True)
+
+    def calculate_skew(self, user_local_date: datetime.date) -> float:
+        """
+        Calculate the overdueness of the task. A value greater than or equal to 1 signifies this task is due.
+        Tasks that are currently deferred have a skew of 0.
+
+        :param user_local_date: the local date of the user in their current time zone
+        :return: the skew of the task, as a decimal
+        """
+        if self.defer_until and user_local_date < self.defer_until:
+            return 0.0
+        return (user_local_date - self.last_completion).days / self.ideal_interval
+
+
+class TaskLog(BaseModel):
+    __tablename__ = "task_logs"
+    id: int = db.Column(db.Integer, primary_key=True)
+    task_id: int = db.Column(db.Integer, db.ForeignKey('tasks.id'), nullable=False)
+    at: datetime.datetime = db.Column(db.DateTime, nullable=False)  # ALWAYS UTC
+    # in what time zone were we when we saved the above UTC timestamp?
+    at_time_zone: str = db.Column(db.String(128), nullable=False)
+    action: str = db.Column(db.Text, nullable=False)
