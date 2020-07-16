8c93761e49d41dedc6f210fc239f912dceac93a9
Robert Sanek <rsanek@gmail.com>
2020-07-16 11:10:32 -0500

    Test out YOuTube autoplay iframe for mobile chrome

--- app/themoviedb.py
+++ app/themoviedb.py
@@ -1,8 +1,10 @@
 import tmdbsimple
 
-from app import kv
+from app import kv #, db
 
 # https://developers.themoviedb.org/3/configuration/get-api-configuration
+# from app.models.tmdb import Video
+
 BASE_URL = 'https://image.tmdb.org/t/p/'
 POSTER_SIZE = 'w500'
 
@@ -15,23 +17,45 @@ def get_stuff():
 
     result = ''
 
-    for movie in acct.rated_movies()['results']:
+    for movie in acct.rated_movies()['results'][1:2]:
         m_id = movie['id']
         title = movie['original_title']
         description = movie['overview']
         image = get_image_url(movie['poster_path'])
         year_released = movie['release_date'][:4]
 
-        m_info = tmdbsimple.Movies(m_id).info()
-        genres = ", ".join([name for _, name in m_info['genres']])
+        movie_detail = tmdbsimple.Movies(m_id)
+        youtube_trailers = [m for m in movie_detail.videos()['results'] if m['site'] == 'YouTube']
+        first_youtube_trailer = ''
+        if youtube_trailers:
+            first_youtube_trailer = youtube_trailers[0]['key']
+        # m_info = movie.info()
+        # genres = ", ".join([name for _, name in m_info['genres']])
 
         m_credits = tmdbsimple.Movies(m_id).credits()
         cast = [f"{cast['name']} as {cast['character']}" for cast in m_credits['cast']][:3]
         cast_output = f"<ul>{''.join(['<li>' + as_string + '</li>' for as_string in cast])}</ul>"
 
-        result += "<br>".join([title + f' ({year_released})', description, cast_output, f"<img src='{image}'>"])
+        # get_or_add_video(Video(
+        #     id=f"zdone:video:tmdb:{m_id}",
+        #     name=title,
+        #     description=description,
+        #     release_date=movie['release_date'],
+        #     youtube_trailer_key=first_youtube_trailer,
+        #     poster_image_url=image,
+        # ))
+
+        result += "<br>".join([title + f' ({year_released})', description, cast_output, f"<img src='{image}'>",
+                               f'<iframe width="560" height="315" src="https://www.youtube.com/embed/{first_youtube_trailer}?controls=0&autoplay=1&mute=1" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'])
 
     return result
+
+
+# def get_or_add_video(video):
+#     maybe_persisted_video = Video.query.filter_by(id=video.id).one_or_none()
+#     if not maybe_persisted_video:
+#         db.session.add(video)
+#         db.session.commit()
 
 
 def get_image_url(path):
