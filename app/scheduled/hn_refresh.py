20b8b7d6edc0196c29f89a69cfb630005c354042
Robert Sanek <rsanek@gmail.com>
2020-08-11 19:29:55 -0500

    Print progress

--- app/scheduled/hn_refresh.py
+++ app/scheduled/hn_refresh.py
@@ -4,12 +4,17 @@ from datetime import datetime
 import requests
 
 from app import kv, db
+from app.log import log
 from app.models.hn import HnStory
 
 if __name__ == '__main__':
     current_item = int(kv.get('HN_ITEM_OFFSET'))
     max_item = int(requests.get('https://hacker-news.firebaseio.com/v0/maxitem.json').text)
+    total_to_do = max_item - current_item
+    log(f"Current max is #{max_item}, which is {total_to_do} ahead of current item #{current_item}.")
     while current_item < max_item:
+        if current_item % 10 == 0:
+            log(f"On item #{current_item}; {round((((max_item - current_item) / total_to_do) - 1) * 100)}% done.")
         item = json.loads(requests.get(
             f"https://hacker-news.firebaseio.com/v0/item/{current_item}.json?print=pretty").text)
         if item and item['type'] == "story" and "deleted" not in item:
