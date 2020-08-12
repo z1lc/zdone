8839324962e7b8ec128ebee221051b65e9dd5998
Robert Sanek <rsanek@gmail.com>
2020-08-11 19:06:10 -0500

    Add HN models in preparation for new reading UI

---
+++ app/models/hn.py
@@ -0,0 +1,21 @@
+import datetime
+
+from app import db
+from app.models.base import BaseModel
+
+
+class HnStory(BaseModel):
+    __tablename__ = "hn_stories"
+    id: int = db.Column(db.Integer, primary_key=True)
+    comments: int = db.Column(db.Integer, nullable=False)
+    score: int = db.Column(db.Integer, nullable=False)
+    title: str = db.Column(db.Text, nullable=False)
+    url: str = db.Column(db.Text, nullable=False)
+    posted_at: datetime.datetime = db.Column(db.DateTime, nullable=False)
+
+
+class HnReadLog(BaseModel):
+    __tablename__ = "hn_read_logs"
+    id: int = db.Column(db.Integer, primary_key=True)
+    user_id: int = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    hn_story_id: int = db.Column(db.Integer, db.ForeignKey('hn_stories.id'), nullable=False)
---
+++ app/scheduled/hn_refresh.py
@@ -0,0 +1,26 @@
+import json
+from datetime import datetime
+
+import requests
+
+from app import kv, db
+from app.models.hn import HnStory
+
+if __name__ == '__main__':
+    current_item = int(kv.get('HN_ITEM_OFFSET'))
+    max_item = int(requests.get('https://hacker-news.firebaseio.com/v0/maxitem.json').text)
+    while current_item < max_item:
+        item = json.loads(requests.get(
+            f"https://hacker-news.firebaseio.com/v0/item/{current_item}.json?print=pretty").text)
+        if item['type'] == "story" and "deleted" not in item:
+            db.session.add(HnStory(
+                id=item['id'],
+                comments=item.get('descendants', 0),
+                score=item['score'],
+                title=item['title'],
+                url=item.get('url', f"https://news.ycombinator.com/item?id={item['id']}"),
+                posted_at=datetime.fromtimestamp(item['time']),
+            ))
+            db.session.commit()
+        current_item += 1
+        kv.put('HN_ITEM_OFFSET', str(current_item))
