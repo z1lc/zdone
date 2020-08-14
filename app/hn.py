bfdf76d722ef4cb9bf6163b22f5ee70b9cb15756
Robert Sanek <rsanek@gmail.com>
2020-08-13 21:33:49 -0500

    humanize time

--- app/hn.py
+++ app/hn.py
@@ -1,14 +1,23 @@
+import json
+from datetime import datetime
 from typing import List
+
+import humanize
 
 from app import db
 from app.models.base import User
 from app.models.hn import HnStory, HnReadLog
+from app.util import JsonDict
 
 
 def get_unread_stories(user: User) -> List[HnStory]:
     read_logs = db.session.query(HnReadLog).filter_by(user_id=user.id).subquery()
-    return db.session.query(HnStory) \
-        .outerjoin(read_logs) \
-        .filter(read_logs.c.id == None) \
-        .filter(HnStory.score >= 100) \
-        .order_by(HnStory.score.desc()).all()  # type:ignore
+    stories = [s for s in db.session.query(HnStory)
+        .outerjoin(read_logs)
+        .filter(read_logs.c.id == None)
+        .filter(HnStory.score >= 100)
+        .order_by(HnStory.score.desc())  # type:ignore
+        .all()]
+    for story in stories:
+        story.posted_at = humanize.naturaltime(datetime.now() - story.posted_at)
+    return stories
