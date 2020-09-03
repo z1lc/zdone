e5bee0087a11337049d7e9d819d1f9df63f3b6e3
Robert Sanek <rsanek@gmail.com>
2020-09-03 10:25:29 -0500

    Refine reminder selection for notifications to have a longer period between re-notifying: 2 -> [2, 14]

--- app/scheduled/send_notifications.py
+++ app/scheduled/send_notifications.py
@@ -3,33 +3,41 @@ import random
 from app import db
 from app.log import log
 from app.models.base import User
+from app.models.tasks import Reminder
 from app.reminders import send_and_log_notification
+
+MINIMUM_DAYS_BETWEEN_NOTIFICATION_FOR_SAME_REMINDER = 2
+MAXIMUM_DAYS_BETWEEN_NOTIFICATION_FOR_SAME_REMINDER = 14
 
 # This code is scheduled to run once daily by the Heroku Scheduler
 if __name__ == '__main__':
     log('Will send a single notification to everyone who has a Pushover user key.')
     for user in User.query.filter(User.pushover_user_key.isnot(None)).all():  # type: ignore
-        # select the reminder with the least total amount of existing notifications, breaking ties by preferring
-        # reminders with the longest time since last notification.
-        # Also, avoid re-sending notifications that you just sent in the last 2 days
-        prepared_sql = f"""with reminder_notifications_joined as (
+        num_active_reminders = Reminder.query.filter_by(user_id=user.id, active=True).count()
+        last_n_to_ignore = max(
+            min(num_active_reminders // 2,
+                MAXIMUM_DAYS_BETWEEN_NOTIFICATION_FOR_SAME_REMINDER),
+            MINIMUM_DAYS_BETWEEN_NOTIFICATION_FOR_SAME_REMINDER)
+
+        valid_potential_reminders_to_be_sent_with_counts = f"""
+with reminder_notifications_joined as (
     select r.id as reminder_id, rn.id as notification_id, user_id, title, active, sent_at
     from reminders r
              left join reminder_notifications rn on r.id = rn.reminder_id
 ),
-    last_two_notifications as (
+    last_n_notifications as (
         select reminder_id
         from reminder_notifications_joined
         where user_id = {user.id} and reminder_id is not null and sent_at is not null
         order by sent_at desc
-        limit 2
+        limit {last_n_to_ignore}
     )
 select reminder_id, title, count(sent_at), coalesce(max(sent_at), '2020-01-01')
 from reminder_notifications_joined
-where active and user_id = {user.id} and reminder_id not in (select * from last_two_notifications)
+where active and user_id = {user.id} and reminder_id not in (select * from last_n_notifications)
 group by 1, 2
 order by 3 asc, 4 asc;"""
-        potential_reminders = list(db.engine.execute(prepared_sql))
+        potential_reminders = list(db.engine.execute(valid_potential_reminders_to_be_sent_with_counts))
         if potential_reminders:
             lowest_number_of_notifications = potential_reminders[0][2]
             potential_reminders = [p for p in potential_reminders if p[2] == lowest_number_of_notifications]
