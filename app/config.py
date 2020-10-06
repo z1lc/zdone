8fc091cf6d13751b0f32487f02125327f17026e3
Robert Sanek <rsanek@gmail.com>
2020-10-05 21:24:44 -0500

    remove fallback to SQLite (migrations don't work on it anyway)

--- app/config.py
+++ app/config.py
@@ -5,6 +5,5 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-                              'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
