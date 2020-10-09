8a4519494a60eb854ecf1aad2bf69d4970022e0d
Robert Sanek <rsanek@gmail.com>
2020-10-09 10:23:48 -0500

    Add best selling artists table

--- app/models/spotify.py
+++ app/models/spotify.py
@@ -93,6 +93,14 @@ class TopTrack(BaseModel):
     )
 
 
+# An artist sourced from https://en.wikipedia.org/wiki/List_of_best-selling_music_artists
+class BestSellingArtist(BaseModel):
+    __tablename__ = "best_selling_artists"
+    id: int = db.Column(db.Integer, primary_key=True)
+    artist_uri: str = db.Column(db.String(128), db.ForeignKey('spotify_artists.uri'), nullable=False, unique=True)
+    claimed_sales: int = db.Column(db.Integer, nullable=False)
+
+
 # genanki uses the `guid` column in Anki's `notes` table to deduplicate upon import. While I have updated the GUID to
 # be based upon the Spotify track URI, old notes that were imported using the original, CSV-based method have GUIDs that
 # will not match these, since Anki seems to assign GUIDs randomly instead of in some deterministic fashion. This table
