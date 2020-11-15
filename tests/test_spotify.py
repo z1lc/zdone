80a6ee4b60dd38f5ada4d490b5c6f97a5b3b1e1c
rsanek <rsanek@gmail.com>
2020-11-14 16:30:55 -0800

    fix import

--- tests/test_spotify.py
+++ tests/test_spotify.py
@@ -2,7 +2,7 @@ import pytest
 
 from app.models.base import User
 from app.spotify import update_spotify_anki_playlist
-from card_generation.spotify import clean_album_name, clean_track_name
+from app.card_generation.spotify import clean_album_name, clean_track_name
 
 
 @pytest.mark.skip(reason="integration")
