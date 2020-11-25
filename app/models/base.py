28167be1fed10a7fb2da26938a429aeb21b02533
rsanek <rsanek@gmail.com>
2020-11-25 14:36:05 -0800

    fix import

--- app/models/base.py
+++ app/models/base.py
@@ -10,7 +10,7 @@ from werkzeug.security import generate_password_hash, check_password_hash
 
 from app import db
 from app import login
-from config import is_ci
+from app.config import is_ci
 
 BaseModel: DefaultMeta = db.Model
 
