57e585ed8caf6704f720290280d5ae7337b28eec
rsanek <rsanek@gmail.com>
2020-11-27 16:08:02 -0800

    Add query to consider creating recurring tasks for

--- scratch.sql
+++ scratch.sql
@@ -194,3 +194,10 @@ from best_selling_artists bsa
 where not following or following is null
 order by claimed_sales desc
 ;
+
+--Completed Trello tasks (non-recurring)
+select at, task_name
+from task_logs
+where user_id = 1 and task_id is null and action = 'complete'
+and at >= '2020-11-26'
+order by at desc
